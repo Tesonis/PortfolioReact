dfbd82fc316e8cb8902a3655d26f4894e50bcbfe
Tesonis <tl_huang@hotmail.com>
2018-10-27 17:13:39 -0400

    Added Bootstrap Template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '../src/css/resume.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Container,
-  Row,
-  Col,
-  Jumbotron,
-  Button
-} from 'reactstrap';
+import Navbar from '../src/sections/navbar.jsx';
+import Home from '../src/sections/Home.jsx';
 
 class App extends Component {
   constructor(props) {
@@ -34,40 +23,8 @@ class App extends Component {
   render() {
       return (
           <div>
-              <Navbar color="inverse" light expand="md">
-                  <NavbarBrand href="/">reactstrap</NavbarBrand>
-                  <NavbarToggler onClick={this.toggle} />
-                  <Collapse isOpen={this.state.isOpen} navbar>
-                      <Nav className="ml-auto" navbar>
-                          <NavItem>
-                              <NavLink href="/components/">Components</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
-                          </NavItem>
-                      </Nav>
-                  </Collapse>
-              </Navbar>
-              <Jumbotron>
-                  <Container>
-                      <Row>
-                          <Col>
-                              <h1>Welcome to React</h1>
-                              <p>
-                                  <Button
-                                      tag="a"
-                                      color="success"
-                                      size="large"
-                                      href="http://reactstrap.github.io"
-                                      target="_blank"
-                                  >
-                                      View Reactstrap Docs
-                                  </Button>
-                              </p>
-                          </Col>
-                      </Row>
-                  </Container>
-              </Jumbotron>
+              <Navbar />
+              <Home/>
           </div>
       );
   }
