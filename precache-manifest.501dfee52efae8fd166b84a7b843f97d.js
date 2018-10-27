3e132e9875d734dc0c1fcfebf33a9e64a4455924
Tesonis <tl_huang@hotmail.com>
2018-10-27 19:00:32 -0400

    Updates

---
+++ precache-manifest.501dfee52efae8fd166b84a7b843f97d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "471dbb9920533a928c3b",
+    "url": "/PortfolioReact/static/js/runtime~main.471dbb99.js"
+  },
+  {
+    "revision": "ed3ba57067ba7568c539",
+    "url": "/PortfolioReact/static/js/main.ed3ba570.chunk.js"
+  },
+  {
+    "revision": "f5b3cefdd9ee36314519",
+    "url": "/PortfolioReact/static/js/1.f5b3cefd.chunk.js"
+  },
+  {
+    "revision": "ed3ba57067ba7568c539",
+    "url": "/PortfolioReact/static/css/main.18fc89dd.chunk.css"
+  },
+  {
+    "revision": "f5b3cefdd9ee36314519",
+    "url": "/PortfolioReact/static/css/1.1be09189.chunk.css"
+  },
+  {
+    "revision": "9a176bea681786740a83d9061e954ede",
+    "url": "/PortfolioReact/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/PortfolioReact/precache-manifest.0358fe78258b036c840f2fc951909d06.js"
+  "/PortfolioReact/precache-manifest.501dfee52efae8fd166b84a7b843f97d.js"
 );
 
 workbox.clientsClaim();
